d4a4243ad251563661a22827f4a47d5c805fe0ff
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-11 21:53:15 -0400

    Multi line comment

--- comment.js
+++ comment.js
@@ -1,2 +1,11 @@
 //Opening line
 console.log('It was love at first site.');
+
+console.log('The first time Yossarian saw the chapplain he fell madly i love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+
+
+console.log('The dictirs were puzzled by the fact that it wasn\'t quit jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('It it didn\'t become jaundice and went away they ciykd discharge him.');
+console.log('But thus just being shirt of juaundice all the time confused them.');
