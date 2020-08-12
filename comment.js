91708245919aed3c5f180c20d887b96daf626e87
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-11 21:54:21 -0400

    add multi line /* */ commment

--- comment.js
+++ comment.js
@@ -1,6 +1,7 @@
-//Opening line
+//Opening line, single line comment 
 console.log('It was love at first site.');
 
+/*
 console.log('The first time Yossarian saw the chapplain he fell madly i love with him.');
 console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
 
@@ -8,4 +9,4 @@ console.log('Yossarian was in the hospital with a pain in his liver that fell ju
 console.log('The dictirs were puzzled by the fact that it wasn\'t quit jaundice.');
 console.log('If it became jaundice they could treat it.');
 console.log('It it didn\'t become jaundice and went away they ciykd discharge him.');
-console.log('But thus just being shirt of juaundice all the time confused them.');
+console.log('But thus just being shirt of juaundice all the time confused them.');*/
