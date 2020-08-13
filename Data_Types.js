e0757e86186a0b62dcb9ac4406d95205cd68a8b6
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-13 14:13:54 -0400

    data types

---
+++ Data_Types.js
@@ -0,0 +1,13 @@
+        //PRIMITIVE DATA TYPE
+//Numbers (-including thos with decimals)
+//Strings (' ' "  ") strings are surrounded by quotes
+//Boolean (True or False)
+//Null ( Intentional abscens of value)
+//Undefined (Represents the absence of value)
+//Symbol (unique identifiers)
+
+        //COMPLEX
+//Object (collection of related data)
+
+
+
