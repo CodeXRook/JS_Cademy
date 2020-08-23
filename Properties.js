c694c8a3f40c6fe0f6d453e394dc3d9f011820b6
DTKPRPT <dtkprpt@DTKPRPT.local>
2020-08-23 17:51:29 -0400

    found Properties length with .length

---
+++ Properties.js
@@ -0,0 +1,6 @@
+/* The . is another operator! We call it the dot operator.
+The value saved to the length property is retrieved from the instance of the string, 'Hello'. 
+The program prints 5 to the console, because Hello has five characters in it.
+*/
+
+console.log('Teaching the world how to code'.length);
